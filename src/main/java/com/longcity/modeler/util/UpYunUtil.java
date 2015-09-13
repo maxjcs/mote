ed9c2afd84d8ed9f632274cbe7b4dc7c31e0ad51
maxjcs <64208531@qq.com>
2015-09-13 13:56:50 +0800

    no message

--- src/main/java/com/longcity/modeler/util/UpYunUtil.java
+++ src/main/java/com/longcity/modeler/util/UpYunUtil.java
@@ -3,11 +3,6 @@
  */
 package com.longcity.modeler.util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
@@ -62,11 +57,6 @@ public class UpYunUtil {
 		return "";
 	}
 	
-	public static void main(String[] args) {
-		//getFileDir();
-		System.out.println(uploadAvartImg("http://xfsr.91xuexibao.com/21,24570ebac9aab5"));
-	}
-	
 	/**
 	 * 获取文件上传路径
 	 * @return
@@ -78,61 +68,5 @@ public class UpYunUtil {
 		System.out.println(fileDir);
 		return fileDir;
 	}
-	
-	/**
-	 * 上传教师历史头像到Upyun
-	 * @param imgUrl
-	 * @return
-	 */
-	public static String uploadAvartImg(String imgUrl){
-		
-		try{
-		
-		URL url = new URL(imgUrl); 
-		HttpURLConnection conn = (HttpURLConnection)url.openConnection();    
-        //设置超时间为3秒  
-        conn.setConnectTimeout(3*1000);  
-        //得到输入流  
-        InputStream inputStream = conn.getInputStream();    
-        //获取自己数组  
-        byte[] getData = readInputStream(inputStream); 
-		// 初始化空间
-		upyun = new UpYun(BUCKET_NAME, OPERATOR_NAME, OPERATOR_PWD);
-		upyun.setDebug(false);
-		
-		String filename=UUID.randomUUID().toString();
-		String extendName="jpg";
-		// 要传到upyun后的文件路径,原文件的路径
-		String filePath = DIR_ROOT + getFileDir()+"/"+filename+"."+extendName;
-		
-		boolean success = upyun.writeFile(filePath, getData, true);
-		if(success){
-			return URL + filePath + "!";
-		}else{
-			System.out.println("--error:"+imgUrl);
-		}
-		}catch(Exception ex){
-			ex.printStackTrace();
-		}
-		return "";
-	}
-	
-	 /** 
-     * 从输入流中获取字节数组 
-     * @param inputStream 
-     * @return 
-     * @throws IOException 
-     */  
-    public static  byte[] readInputStream(InputStream inputStream) throws IOException {    
-        byte[] buffer = new byte[1024];    
-        int len = 0;    
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();    
-        while((len = inputStream.read(buffer)) != -1) {    
-            bos.write(buffer, 0, len);    
-        }    
-        bos.close();    
-        return bos.toByteArray();    
-    }
-	
 
 }
